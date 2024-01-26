b3ab70f66bcdb6128e4a67cb98c3b61b495ca75a
unknown <you@example.com>
2024-01-26 15:14:51 +0800

    input done

--- base_raise_to_power.py
+++ base_raise_to_power.py
@@ -6,8 +6,10 @@ def exponent(base, exp):
     return result
 
 # Enter base
+base = int(input("Please enter the base: "))
 
 # Enter exponent
+exp = int(input("Please enter the exponent: "))
 
 # Call the function
 
